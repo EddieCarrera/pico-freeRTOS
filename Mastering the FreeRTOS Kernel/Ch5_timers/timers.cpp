304797010c23d5fbcb3a6eb13924418dce684cfb
EddieCarrera <Carrera318a@gmail.com>
2023-09-07 22:27:16 -0700

    Chapter 5 WIP

--- Mastering the FreeRTOS Kernel/Ch5_timers/timers.cpp
+++ Mastering the FreeRTOS Kernel/Ch5_timers/timers.cpp
@@ -4,14 +4,71 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
+// #define UNIQUE_CALLBACK_EXAMPLE
+#define SINGLE_CALLBACK_TIMERID_EXAMPLE
+
 /***************************** Important Notes *****************************
  * 1) By default, the max timers available in the deamon queue is set by 
  * configTIMER_QUEUE_LENGTH. The default priority is configMAX_PRIORITIES-1. 
+ * 
+ * 2) The pvTimerID field can be used by the application writer for any 
+ * purpose. It seems like a callback would be the most practical application.
+ * The same callback function can be assigned to more than one software timer. 
+ * When that is done, the callback function parameter is used to determine 
+ * which software timer expired.
+ * 
+ * 3) Unlike other software timer API functions, vTimerSetTimerID() and 
+ * pvTimerGetTimerID() access the software timer directly—they do not send 
+ * a command to the timer command queue.
 *****************************************************************************/
 
 #define main_ONESHOT_TIMER_PERIOD   pdMS_TO_TICKS(3333)
 #define main_RELOAD_TIMER_PERIOD    pdMS_TO_TICKS(500)
 
+TimerHandle_t autoReload_timer, oneShot_timer;
+BaseType_t timer1Started, timer2Started;
+
+/* prvTimerCallback() will execute when either timer expires. 
+ * The implementation of prvTimerCallback() uses the function’s parameter to 
+ * determine if it was called because the one-shot timer expired, or 
+ * because the auto-reload timer expired. prvTimerCallback() also 
+ * demonstrates how to use the software timer ID as timer specific storage; 
+ * each software timer keeps a count of the number of times it has expired 
+ * in its own ID, and the auto-reload timer uses the count to stop itself 
+ * the fifth time it executes. */
+#ifdef SINGLE_CALLBACK_TIMERID_EXAMPLE
+void prvTimerCallback(TimerHandle_t currTimer)
+{
+    TickType_t timeNow;
+    uint32_t ctr;
+
+    /* A count of the number of times this software timer has expired is 
+    stored in the timer's ID. Obtain the ID, increment it, then save it as
+    the new ID value. The ID is a void pointer, so is cast to a uint32_t. */
+    ctr = (uint32_t)pvTimerGetTimerID(currTimer);
+    vTimerSetTimerID(currTimer, (void*)(++ctr));
+
+    timeNow = xTaskGetTickCount();
+
+    if (currTimer == oneShot_timer)
+    {
+        printf("One-shot timer callback executing: %u\n", timeNow);
+    }
+    else {
+        printf("Auto-reload timer callback executing: %u\n", timeNow);
+
+        if (ctr++ == 5)
+        {
+            /* This callback function executes in the context of the RTOS 
+               daemon task so must not call any functions that might place 
+               the daemon task into the Blocked state. Therefore a block time 
+               of 0 is used. */
+            xTimerStop(currTimer, 0);
+        }
+    }
+}
+#endif
+#ifdef UNIQUE_CALLBACK_EXAMPLE
 void pvOneShotTimerCallback(TimerHandle_t xTimer)
 {
     TickType_t timeNow;
@@ -25,15 +82,32 @@ void pvAutoReloadTimerCallback(TimerHandle_t xTimer)
     timeNow = xTaskGetTickCount();
     printf("Auto-reload timer callback executing: %u\n", timeNow);
 }
+#endif
 
 int main (void)
 {
-    TimerHandle_t autoReload_timer, oneShot_timer;
-    BaseType_t timer1Started, timer2Started;
-
     stdio_init_all();
     while(!stdio_usb_connected()){tight_loop_contents();}
 
+#ifdef SINGLE_CALLBACK_TIMERID_EXAMPLE
+    oneShot_timer = xTimerCreate(
+        "OneShot",
+        main_ONESHOT_TIMER_PERIOD,
+        pdFALSE,
+        0,  // Initialize timerID as 0
+        prvTimerCallback
+    );
+
+    autoReload_timer = xTimerCreate(
+        "autoReload",
+        main_RELOAD_TIMER_PERIOD,
+        pdTRUE,
+        0,  // Initialize timerID as 0
+        prvTimerCallback
+    );   
+#endif
+
+#ifdef UNIQUE_CALLBACK_EXAMPLE
     oneShot_timer = xTimerCreate(
         "OneShot",
         main_ONESHOT_TIMER_PERIOD,
@@ -49,7 +123,7 @@ int main (void)
         NULL,
         pvAutoReloadTimerCallback
     );    
-
+#endif
     /* Check the software timers were created. */
     if( ( oneShot_timer != NULL ) && ( autoReload_timer != NULL ) ) 
     {
