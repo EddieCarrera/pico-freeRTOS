e73277785462ad4a2931e5182024144b1b1cb7a3
Eddie Carrera <Carrera318a@gmail.com>
2023-09-19 20:37:25 +0000

    Chapter 6 WIP Example 19

--- Mastering the FreeRTOS Kernel/Ch6_interrupt_handling/gpioInterrupt_isrQueues.cpp
+++ Mastering the FreeRTOS Kernel/Ch6_interrupt_handling/gpioInterrupt_isrQueues.cpp
@@ -35,5 +35,101 @@
  * 
  * Processing the received characters directly within the ISR, then using a queue 
  * to send just the result of processing the data (rather than the raw data) to a 
- * task. 
+ * task.
+ * 
+ * 2) With regards to portYIELD_FROM_ISR()
+ * The interrupt will always return to the task in the Running state, even if the 
+ * task in the Running state changed while the interrupt was executing. 
  *******************************************************************************/
+
+#define GPIO_PIN        9
+#define TX_PERIOD_MS    5000
+
+QueueHandle_t intQueue;
+QueueHandle_t stringQueue;
+BaseType_t full = false;
+
+void periodic_NumToISR_task(void *pvParameter)
+{
+    static uint32_t ctr = 0;
+    TickType_t prevTime = xTaskGetTickCount();
+
+    while(1)
+    {
+        vTaskDelayUntil( &prevTime, pdMS_TO_TICKS(TX_PERIOD_MS) );
+        if (xQueueSendToBack( intQueue, &ctr, 0 ) != errQUEUE_FULL)
+        {
+            ctr = ctr > 5 ? ctr = 0 : ctr++;
+        }
+    }
+}
+
+void stringFromISR_task(void *pvParameter)
+{
+    char *rxString;
+    while(1)
+    {
+        /* Block on the queue to wait for data to arrive. */
+        xQueueReceive( stringQueue, &rxString, portMAX_DELAY );
+        
+        /* Print out the string received */
+        printf("%s", rxString);
+    }
+}
+
+void gpio_callback(uint gpio, uint32_t events)
+{
+    uint32_t uReceivedNumber = 0;
+
+    // Defines an array of pointers to strings
+    static const char *strings[] =
+    {
+        "String1\r\n",
+        "String2\r\n",
+        "String3\r\n",
+        "String4\r\n"
+        "String5\r\n"
+    };
+    
+    /* As always, xHigherPriorityTaskWoken is initialized to pdFALSE to be able to
+    detect it getting set to pdTRUE inside an interrupt safe API function. Note that
+    as an interrupt safe API function can only set xHigherPriorityTaskWoken to
+    pdTRUE, it is safe to use the same xHigherPriorityTaskWoken variable in both
+    the call to xQueueReceiveFromISR() and the call to xQueueSendToBackFromISR(). */
+    BaseType_t xHigherPriorityTaskWoken = pdFALSE;
+
+    printf("ISR triggered\r\n");
+
+    // event is an enum gpio_irq_level
+    if (gpio == GPIO_PIN)
+    {
+        /* Read from the queue until the queue is empty. */
+        while (xQueueReceiveFromISR(xIntegerQueue, 
+                                    &uReceivedNumber,
+                                    &xHigherPriorityTaskWoken) != errQUEUE_EMPTY)
+        {
+            xQueueSendToBackFromISR(xStringQueue,
+                                    &strings[uReceivedNumber],
+                                    &xHigherPriorityTaskWoken );
+        }
+    } // if (gpio == GPIO_PIN)
+
+    portYIELD_FROM_ISR( xHigherPriorityTaskWoken );
+}
+
+int main()
+{
+    stdio_init_all();
+    while(!stdio_usb_connected()){tight_loop_contents();}
+
+    sleep_ms(1000);
+    printf("ISR Queues Example\r\n");
+    gpio_pull_up(GPIO_PIN);
+    gpio_set_irq_enabled_with_callback(GPIO_PIN, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+
+    /* Start the scheduler so the created tasks start executing. */
+    vTaskStartScheduler();
+
+    /* As normal, the following line should never be reached. */
+    for( ;; );
+}
